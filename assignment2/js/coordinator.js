194556785f422cfda20881f7f758762f5391ca8e
The Glitch <theglitch@localhost.localdomain>
2016-04-08 23:55:38 +0300

    updated

--- assignment2/js/coordinator.js
+++ assignment2/js/coordinator.js
@@ -1,12 +1,29 @@
 /**
  * Created by root on 4/6/16.
  */
+'use strict';
 $(document).ready(function(){
-   let url = 'http://localhost:9090/api/requests';
-   fetch(url).then(response=>response.json()).then(requests=>{
-       console.log(requests);
-       let htmlTemplate = $('#requests-template').html(),
-           requestsTemplate = Handlebars.compile(htmlTemplate);
-       $('#requests-table').html(requestsTemplate({requests}));
-   });
+    let username = getParameterByName('username');
+    let userFetcher = 'http://localhost:9090/api/users/'+username;
+    fetch(userFetcher).then(res=>res.json()).then(a=>{
+        let fullname=a.firstname+' '+a.lastname;
+        document.getElementById('userFullname').innerHTML = fullname;
+    });
+    let url = 'http://localhost:9090/api/requests';
+    fetch(url).then(response=>response.json()).then(requests=>{
+        console.log(requests);
+        let htmlTemplate = $('#requests-template').html(),
+            requestsTemplate = Handlebars.compile(htmlTemplate);
+        $('#requests-table').html(requestsTemplate({requests}));
+    });
+    console.log(username);
 });
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
--- assignment2/js/login.js
+++ assignment2/js/login.js
@@ -6,10 +6,19 @@ function doLogin(){
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
     let url = 'http://localhost:9090/api/users/'+username;
+    let div = document.createElement("div");
+    div.style.color = "red";
+    div.style.size = "100%";
     fetch(url).then(res=>res.json()).then(user=>{
         if(user.hasOwnProperty('type')){
-            window.location="http://localhost:9090/coordinator.html";
+            if(user.password===password)
+                window.location="http://localhost:9090/coordinator.html?username="+user.username;
+            else document.getElementById('invalid').innerHTML = 'Invalid Username or Password!';
         }
-        else window.location="http://localhost:9090/student.html";
+        else{
+            if(user.password===password)
+                window.location="http://localhost:9090/coordinator.html?username="+user.username;
+            else document.getElementById('invalid').innerHTML = 'Invalid Username or Password!';
+        }
     });
 }
