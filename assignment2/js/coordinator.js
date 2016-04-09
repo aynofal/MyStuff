c4d2341069d5ea8b4d64444f79c0293ec85b01f1
The Glitch <theglitch@localhost.localdomain>
2016-04-09 20:11:21 +0300

    changes

--- assignment2/js/coordinator.js
+++ assignment2/js/coordinator.js
@@ -9,11 +9,17 @@ $(document).ready(function(){
         let fullname=a.firstname+' '+a.lastname;
         document.getElementById('userFullname').innerHTML = fullname;
         let url = 'http://localhost:9090/api/requests';
-        fetch('http://localhost:9090/api/sections/'+a.staffNo).then(sections=>{
-            console.log(kkkk);
+        fetch('http://localhost:9090/api/sections/'+a.staffNo).then(sections=>sections.json()).then(sections=>{
             fetch(url).then(response=>response.json()).then(requests=>{
-                requests=requests.filter(r=>r.crn===sections.crn);
+                requests=requests.filter(r=>{
+                    for(let i=0; i<sections.length; i++){
+                        console.log(sections[i]);
+                        if(sections[i].crn==r.crn)
+                            return r;
+                    }
+                });
                 console.log(requests);
+                console.log('hello!');
                 let htmlTemplate = $('#requests-template').html(),
                     requestsTemplate = Handlebars.compile(htmlTemplate);
                 $('#requests-table').html(requestsTemplate({requests}));
@@ -21,6 +27,7 @@ $(document).ready(function(){
         });
     });
 });
+
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
